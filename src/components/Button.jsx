87358e3bfb60e63c13e85f09b831d0581e0e1e45
Yueling Liu <stellaliuca@gmail.com>
2024-06-25 15:26:56 -0700

    quality

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,11 +2,13 @@ const Button = ({ label, iconURL }) => {
   return (
     <button className="flex justify-center items-center px-7 py-4 border rounded-full bg-coral-red gap-2 font-montserrat text-lg leading-none text-white border-coral-red">
       {label}
-      <img
-        src={iconURL}
-        alt="arrow right icon"
-        className="ml-2 w-5 h-5 rounded-full "
-      />
+      {iconURL && (
+        <img
+          src={iconURL}
+          alt="arrow right icon"
+          className="ml-2 w-5 h-5 rounded-full "
+        />
+      )}
     </button>
   );
 };
--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,5 +1,44 @@
+import Button from '../components/Button';
+import { shoe8 } from '../assets/images';
 const SuperQuality = () => {
-  return <div>SuperQuality</div>;
+  return (
+    <section
+      id="about us"
+      className="flex justify-between items-center max-lg:flex-col gap-10 w-full max-container"
+    >
+      <div className="flex flex-1 flex-col ">
+        <h2 className="font-palanquin text-4xl capitalize font-bold lg:max-w-lg">
+          we provide You <span className="text-coral-red ">super </span>
+          <span className="text-coral-red ">Qaulity </span>
+          Sheos
+        </h2>
+
+        <p className="mt-4 lg:max-w-lg info-text">
+          Nike shoes are synonymous with super quality, offering exceptional
+          durability and unmatched comfort. Each pair is crafted using premium
+          materials and cutting-edge technology to ensure they withstand
+          rigorous use while maintaining peak performance.
+          <br />
+        </p>
+        <p className="mt-6 lg:max-w-lg info-text">
+          Our dedication to detial and excellence ensures your satisfaction
+        </p>
+        <div className="mt-11">
+          <Button label="View Details" className="mt-6" />
+        </div>
+      </div>
+
+      <div className="flex-1 flex justify-center items-center">
+        <img
+          src={shoe8}
+          alt="shoe8"
+          width={570}
+          height={522}
+          className="object-contain"
+        />
+      </div>
+    </section>
+  );
 };
 
 export default SuperQuality;
